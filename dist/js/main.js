f7d6efc79c09e9502c5ce60e4e91195aceb5ef58
Pavel Proyavin <pasha.proyavin@gmail.com>
2019-07-16 17:54:47 +0300

    init

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        
+
+    })
+})(jQuery);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCQpIHtcclxuICAgIFwidXNlIHN0cmljdFwiXHJcbiAgICAkKGZ1bmN0aW9uKCkge1xyXG5cclxuICAgICAgICBcclxuXHJcbiAgICB9KVxyXG59KShqUXVlcnkpOyJdfQ==
